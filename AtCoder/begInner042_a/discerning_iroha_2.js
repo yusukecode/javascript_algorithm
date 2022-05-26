4426a1549f007131ae5dc97a490146bd0ffb9d22
yusukesakamoto <kataru23@gmail.com>
2022-05-26 12:32:21 +0900

    Update once.

---
+++ AtCoder/begInner042_a/discerning_iroha_2.js
@@ -0,0 +1,17 @@
+//https://atcoder.jp/contests/abc042/tasks/arc058_a
+//let lines = require("fs").readFileSync("/dev/stdin", "utf8").split("\n"); //['3 3','dxx','axx','cxx']
+//let score_pr = lines[0].split(' ').map(Number);
+
+let lines = ["1000 8",1,3,4,5,6,7,8,9];
+let score_pr = lines[0].split(' ').map(Number);
+let score_pr_num = lines[1].split(' ').map(Number);
+
+let N = score_pr[0]; //値段
+let K = score_pr[1]; //嫌いな数字の数
+
+for(i = N;i<10001;i++){
+    let 
+}
+
+
+
