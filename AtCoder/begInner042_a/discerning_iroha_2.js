1e8be29a73e5f17a938ccff993e3a6ca7d11c7a5
yusukesakamoto <kataru23@gmail.com>
2022-05-26 14:57:13 +0900

    I think it fits,but it's incorrect.

--- AtCoder/begInner042_a/discerning_iroha_2.js
+++ AtCoder/begInner042_a/discerning_iroha_2.js
@@ -1,17 +1,28 @@
 //https://atcoder.jp/contests/abc042/tasks/arc058_a
-//let lines = require("fs").readFileSync("/dev/stdin", "utf8").split("\n"); //['3 3','dxx','axx','cxx']
-//let score_pr = lines[0].split(' ').map(Number);
 
-let lines = ["1000 8",1,3,4,5,6,7,8,9];
+//let lines = ["1000 8","1 3 4 5 6 7 8 9"];
+let lines = require("fs").readFileSync("/dev/stdin", "utf8").split("\n");
+
 let score_pr = lines[0].split(' ').map(Number);
 let score_pr_num = lines[1].split(' ').map(Number);
+let ans;
 
-let N = score_pr[0]; //値段
-let K = score_pr[1]; //嫌いな数字の数
+let N = score_pr[0];
 
 for(i = N;i<10001;i++){
-    let 
+    ans = i;
+    let flg = true;
+
+    for(let j of score_pr_num){
+        if(String(ans).indexOf(j) != -1){
+            flg=false;
+            break
+        }
+    }
+    if(flg===true){
+        console.log(ans);
+        break
+    } else {
+        continue
+    }
 }
-
-
-
