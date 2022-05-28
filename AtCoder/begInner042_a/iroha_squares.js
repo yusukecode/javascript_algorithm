39f5b1f2eb1d5f26c1bec5741bae25b344543063
yusukesakamoto <kataru23@gmail.com>
2022-05-28 11:20:28 +0900

    It's very dificult.

--- AtCoder/begInner042_a/iroha_squares.js
+++ AtCoder/begInner042_a/iroha_squares.js
@@ -1,16 +1,21 @@
-let lines = ["100000 100000 44444 55555"]; //答え　738162020
+let lines = ["2 3 1 1"]; //答え　738162020
 
 //let lines = require("fs").readFileSync("/dev/stdin", "utf8").split("\n");
 
 let score_pr = lines[0].split(' ').map(Number);
-let ans;
+const[H,W,A,B] = score_pr;
 
-function factirial(n){
-    if(n>1){
-        n = n * (n -1);
-        factirial(n);
-    } else {
-        return n
+function factirial(k) {
+    var j = 1;
+    for(var i = 1; i <= k; i++){
+      j *= i;
     }
-}
+    return j;
+  }
+
+let sc = H + W;
+let all_square = factirial(H+W) / (factirial(H)*factirial(W));
+let sub_square = factirial(A+B) / (factirial(A)*factirial(B));
+
+console.log(all_square - sub_square);
 
