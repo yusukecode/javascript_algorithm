b6d4b51ae683be1cd29f48072d504c1c6145bade
yusukesakamoto <kataru23@gmail.com>
2022-05-29 17:51:49 +0900

    I think it fits in the test,but I get an error when submitting.

---
+++ AtCoder/beginner043/binary_B.js
@@ -0,0 +1,22 @@
+
+//lines = '0BB1';
+//key_arr = [...lines];
+//console.log(key_arr); //[ '0', 'B', 'B', '1' ]
+
+let lines = require("fs").readFileSync("/dev/stdin", "utf8");
+lines = lines.join('');
+key_arr = [...lines];
+tmp = [];
+num = 0;
+for(i=0;i<key_arr.length;i++){
+    if(key_arr[i] === 'B' && tmp[num-1] === ''|| key_arr[i] === 'B' && num === 0){
+    } else if(key_arr[i] === 'B' && tmp[num-1] != '' && num > 0){
+        num -= 1;
+        tmp[num] = '';
+    } else {
+        tmp[num] = key_arr[i];
+        num += 1;
+    }
+}
+
+console.log(tmp.join(''))
